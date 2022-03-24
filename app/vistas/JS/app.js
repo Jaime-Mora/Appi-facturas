86ac9d6f7634bf457db4dd4baff8e2c6895c4a10
jaime mora <Jmora12@ucol.mx>
2022-03-23 21:45:31 -0600

    traer informacion de productos al body

--- app/vistas/JS/app.js
+++ app/vistas/JS/app.js
@@ -4,7 +4,7 @@ btnCargar.addEventListener("click", () => {
     .then(response => response.json())
     .then(json => {
         const Slclientes = document.getElementById("Slclientes")
-        let options=""
+        let options="";
         for (let i = 0; i<json.length; i++) {
            options += `<option value='${json[i].id}'>${json[i].Name}</option>`
         }
@@ -12,4 +12,18 @@ btnCargar.addEventListener("click", () => {
     })   
 });
 
-//Crear 
+
+
+const Btncargar = document.getElementById("Btncargar"); //carga el precio y cantidad de los productos
+Btncargar.addEventListener("click", () => {
+    fetch('http://localhost:1339/api/product')
+  .then(response => response.json())
+  .then(json => {
+      const data = document.getElementById("data")
+      let body = ''
+      for (let i = 0; i < json.length; i++) {
+         body += `<tr><td>${json[i].id}</td><td>${json[i].name}</td><td>${json[i].quantity}</td><td>${json[i].cost}</td></tr>`
+    }
+        document.getElementById("data").innerHTML = body;
+  });
+});
